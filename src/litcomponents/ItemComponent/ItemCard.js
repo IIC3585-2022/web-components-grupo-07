e0ae84fdb8c4b8a20b2bfeb2912b5ddd224279d7
Matías Cadile <mjcadile@uc.cl>
2022-06-15 10:50:05 -0400

    added styles in ItemCard Lit Comp

--- src/litcomponents/ItemComponent/ItemCard.js
+++ src/litcomponents/ItemComponent/ItemCard.js
@@ -1,9 +1,5 @@
 //const { LitElement } = require("lit-element");
-import {
-  html,
-  LitElement,
-  css,
-} from "https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module";
+import { LitElement, html } from "lit-html";
 
 class ItemCardLitComponent extends LitElement {
   static get properties() {
@@ -31,6 +27,76 @@ class ItemCardLitComponent extends LitElement {
 
   render() {
     return html`
+      <style>
+        button {
+          min-width: 7rem;
+          height: 1.8rem;
+          border-radius: 0.5rem;
+          border: none;
+          background-color: ${this.buttonColor};
+          color: ${this.buttonTextColor};
+          font-weight: 300;
+          font-size: 0.7rem;
+          font-weight: bold;
+        }
+        img {
+          width: 100%;
+          min-height: 100%;
+        }
+        .card-cont {
+          font-family: Segoe UI, sans-serif;
+          font-size: 0.98rem;
+          position: relative;
+          background-color: none;
+          width: 20.3rem;
+          height: 20rem;
+          color: #9a1032;
+        }
+        .img-cont {
+          background-color: grey;
+          width: 15rem;
+          height: 15rem;
+          overflow: hidden;
+          border-radius: 1rem;
+          box-shadow: 30px 30px 50px rgba(1, 1, 1, 0.3);
+        }
+        .info-cont {
+          position: absolute;
+          background-color: white;
+          width: 13rem;
+          top: 13rem;
+          left: 2rem;
+          border-radius: 0.6rem;
+          padding: 0.5rem 1rem;
+          box-shadow: 0px 0px 8px rgba(1, 1, 1, 0.3);
+          height: 8.5rem;
+          box-shadow: 30px 30px 50px rgba(1, 1, 1, 0.3);
+        }
+        .top-info-cont {
+          display: flex;
+          justify-content: center;
+          min-height: 50%;
+          width: 100%;
+        }
+        .bottom-tag-cont {
+          display: flex;
+          justify-content: center;
+          align-items: right;
+          width: 50%;
+        }
+        .main-cont {
+          display: flex;
+          font-weight: 700;
+          width: 40%;
+        }
+        .description {
+          font-size: 0.65rem;
+          text-align: left;
+          font-weight: bold;
+          width: 100%;
+          color: #707070;
+        }
+      </style>
       <div class="card-cont">
         <div class="img-cont">
           <img src="${this.src}" alt="${this.alt}" />
